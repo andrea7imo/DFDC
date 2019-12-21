49e5fe1b244e793c1f0eb3708b1154e7959c830f
andrea <settimo.andreasettimo.andrea@gmail.com>
2019-12-21 11:31:57 +0100

    todo list
    Ho inserito una todo list provisoria in questo file. Se avete voglia inseritene altre.

---
+++ Dataset.py
@@ -0,0 +1,9 @@
+# TODO: scrivere una classe per l'import del dataset;
+# TODO: definire gli split in TRAINING, VALIDATION e TEST SET;
+# TODO: creare i dataloader;
+# TODO: creare una funzione per il caricamento dei pesi per la rete (vedi codice deepfake);
+# TODO: creare una funzione per salvare lo stato della rete;
+# TODO: creare una funzione per ripristinare lo stato della rete;
+# TODO: creare funzione per la selezione degli hyperparameters -> grid o random search;
+# TODO: creare funzione per il training;
+# TODO: creare la funzione di testing;
