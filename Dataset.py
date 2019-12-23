4a0a050f5f4c16bce2bee976ff9bfee813cca22d
letol <tolomei.leonardo@gmail.com>
2019-12-23 12:39:27 +0100

    Added loadJSONs()

--- Dataset.py
+++ Dataset.py
@@ -146,6 +146,7 @@ def extractFrames(video_path, output_path, start_frame=0, end_frame=None, count=
 
 def storeFrame(pathROOT, pathJSONInput, pathOutput):
     # estrapolazione di frame dove ci sono i volti da ogni video
+    print("Loaded JSON: {}".format(pathJSONInput))
     with open(pathJSONInput) as f:
         data = json.load(f)
 
@@ -153,12 +154,32 @@ def storeFrame(pathROOT, pathJSONInput, pathOutput):
     args = []
 
     for count, key in enumerate(data, 1):
-        args.append((pathROOT + "/" + key, pathOutput + "/" + data[key]["set"] + "/" + data[key]["label"], 0, None, count, len(data)))
+        set_dir = os.path.join(pathOutput, data[key]["split"])
+        if not os.path.isdir(set_dir):
+            os.mkdir(set_dir)
+
+        label_dir = os.path.join(set_dir, data[key]["label"])
+        if not os.path.isdir(label_dir):
+            os.mkdir(label_dir)
+
+        args.append((os.path.join(pathROOT, key), label_dir, 0, None, count, len(data)))
 
     p.starmap(extractFrames, args)
 
 
+def loadJSONs(pathJSONs, pathOutput):
+    # apertura dei JSON con ricerca in profondità
+
+    if not os.path.isdir(pathOutput):
+        os.mkdir(pathOutput)
+
+    for dir_path, dir_names, file_names in os.walk(pathJSONs):
+        [storeFrame(dir_path, os.path.join(dir_path, file), pathOutput) for file in file_names if file.endswith('.json')]
+
+
 # storeFrame("/aiml/project/DFDC/Datasets/v01a/fb_dfd_release_0.1_final", "/aiml/project/DFDC/Datasets/v01a/fb_dfd_release_0.1_final/dataset.json", "/aiml/project/DFDC/FramesDataset_prova")
 
-train = Dataset("/aiml/project/DFDC/FramesDataset/train")
-print(f"Len trian: {len(train)}")
+loadJSONs("/aiml/project/DFDC/Datasets/dfdc_train", "/aiml/project/DFDC/FramesDataset_full")
+
+# train = Dataset("/aiml/project/DFDC/FramesDataset/train")
+# print(f"Len trian: {len(train)}")
