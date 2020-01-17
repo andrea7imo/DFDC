0f87af4b24a9fa13ef28b879120096edc77b5ebc
andrea <settimo.andreasettimo.andrea@gmail.com>
2020-01-17 18:19:38 +0100

    aggiunta F-1 score
    capire come usarla

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ train_dataloader = DataLoader(training_set, batch_size=BATCH_SIZE, shuffle=True,
 # introdurre il validation set
 
 # Prova di training
-#model = loadModelDeepForecies()
-#prepareTraining(model)
-#train(model, train_dataloader, train_dataloader)    # <-- mettere il validation set nell'ultimo argomento
+model = loadModelDeepForecies()
+prepareTraining(model)
+train(model, train_dataloader, train_dataloader)    # <-- mettere il validation set nell'ultimo argomento
 
 # Prova di hyperparameters optimization
-randomSearchCoarse(train_dataloader, train_dataloader)
+'''randomSearchCoarse(train_dataloader, train_dataloader)
 randomSearchFine(train_dataloader, train_dataloader)
 
 # Prova di visualizzazione dei risultati del hyperparameters optimization
@@ -48,3 +48,4 @@ i_max = avg_accuracy_list.index(max(avg_accuracy_list))
 LR = LR_list[i_max]
 WEIGHT_DECAY_list = WEIGHT_DECAY_list[i_max]
 STEP_SIZE = STEP_SIZE_list[i_max]
+'''
